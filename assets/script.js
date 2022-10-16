06c21ecb9e3a9d9e468e85c36adead51b853ad5c
Sabrina Nawaz <sabrinanawaz23@gmail.com>
2022-10-16 19:38:38 -0400

    Added styling for previous and current operand on CSS, added JavaScript classes on HTML, created variables in script.js and added a class with a constructor

---
+++ assets/script.js
@@ -0,0 +1,14 @@
+const numberButtons = document.querySelectorAll('[data-number]')
+const operationButtons = document.querySelectorAll('[data-operation]')
+const equalsButton = document.querySelector('[data-equals]')
+const deleteButton = document.querySelector('[data-delete]')
+const allClearButton = document.querySelector('[data-all-clear]')
+const previousOperandTextEl = document.querySelector('[data-previous-operand]')
+const currentOperandTextEl = document.querySelector('[data-current-operand]')
+
+class Calculator {
+    constructor(previousOperandTextEl, currentOperandTextEl)
+    this.previousOperandTextEl = previousOperandTextEl
+    this.currentOperandTextEl = currentOperandTextEl
+    this.clearInterval()
+}
